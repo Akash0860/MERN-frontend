f4507061f32551a5c224f48345f0aa9a402baaf5
Akash S <akashaki0860@gmail.com>
2023-05-14 18:20:46 +0530

    initial commit

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL:"https://restuarantreview(mern)/api/v1/restaurants.onrender.com",
+    baseURL:"http://localhost:5000/api/v1/restaurants",
     headers:{
         "Content-Type":"application/json"
     }
